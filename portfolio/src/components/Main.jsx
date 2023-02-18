04db42b01ba4c418812f1c0d9bbd6e592c0edd41
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-02-18 23:29:35 +0530

    Main implemented

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import main from "../assets/back.png";
-import {  FaGithub } from 'react-icons/fa'
-import { FiMail } from 'react-icons/fi';
-import { AiOutlineLinkedin } from 'react-icons/ai';
+import {  FaLinkedinIn } from 'react-icons/fa'
+import { FiGithub } from 'react-icons/fi';
+import { SiGmail } from 'react-icons/si';
+
 
 import { TypeAnimation } from 'react-type-animation'
 
@@ -12,16 +13,16 @@ const Main = () => {
             <img className='w-full h-screen object-cover object-left scale-x-[-1]' src={main} />
             <div className='w-full h-screen absolute top-0 left-0 bg-black/50'>
                 <div className=' max-w-[1000px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center '>
-                    <h1 className='sm-text-6xl text-5xl font-bold text-gray-500'>I'm Chanukya Serasinghe</h1>
-                    <h2 className='flex sm:text-2xl text-2xl pt-4 text-gray-500 '>I'm a
+                    <h1 className='sm-text-6xl text-5xl font-bold text-gray-400'>I'm Chanukya Serasinghe</h1>
+                    <h2 className='flex sm:text-2xl text-2xl pt-4 text-gray-400 '>I'm a
                     <TypeAnimation
                             sequence={[
                                 'Tech enthusiast', // Types 'One'
-                                3000, // Waits 1s
+                                2000, // Waits 1s
                                 'Software Engineering undergrad', // Deletes 'One' and types 'Two'
-                                3000, // Waits 2s
+                                2000, // Waits 2s
                                 'Tutor', // Types 'Three' without deleting 'Two'
-                                3000, // Waits 2s
+                                2000, // Waits 2s
                             ]}
                             wrapper="div"
                             cursor={true}
@@ -29,10 +30,10 @@ const Main = () => {
                             style={{fontSize: '1em', paddingLeft: '5px' }}
                         />
                     </h2>
-                    <div className='flex justify-between pt-6 max-w-[200px] w-full' >
-                        <AiOutlineLinkedin color='white' className='cursor-pointer' size={20} />
-                        <FaGithub color='white' className=' cursor-pointer' size={20}/>
-                        <FiMail color='white' className=' cursor-pointer' size={20}/>
+                    <div className='flex justify-between pt-6 max-w-[150px] w-full' >
+                        <FaLinkedinIn color='white' className='cursor-pointer' size={20} />
+                        <FiGithub color='white' className=' cursor-pointer' size={20}/>
+                        <SiGmail color='white' className=' cursor-pointer' size={20}/>
                         
                     </div>
                 </div>
