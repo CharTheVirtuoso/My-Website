d7078c9c17dd08a1abd8c2fd5b34cf46d5f380dc
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-02-19 10:41:11 +0530

    minor changes

--- portfolio/src/components/SideNav.jsx
+++ portfolio/src/components/SideNav.jsx
@@ -17,28 +17,28 @@ const SideNav = () => {
         <FcMenu size={25} onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
     {
         nav? (
-            <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                <a href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
+            <div className='fixed w-full h-screen bg-gradient-to-r  from-black  to-gray-900 flex flex-col justify-center items-center z-20'>
+                <a onClick={handleNav} href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
                     <AiOutlineHome size={20}/>
                     <span className='pl-4'>Home</span>
 
                 </a>
-                <a href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
+                <a onClick={handleNav} href="#education" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
                     <HiAcademicCap size={20}/>
                     <span className='pl-4'>Education</span>
 
                 </a>
-                <a href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
+                <a onClick={handleNav} href="#projects" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
                     <AiOutlineFundProjectionScreen size={20}/>
                     <span className='pl-4'>Projects</span>
 
                 </a>
-                <a href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
+                <a onClick={handleNav} href='/CMS_Resume.pdf' className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
                     <BsPerson size={20}/>
                     <span className='pl-4'>Resume</span>
 
                 </a>
-                <a href="#home" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
+                <a onClick={handleNav} href="#contact" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-300 shadow-gray-400 m-2 p-4 cursor hover:scale-110 ease-in duration-200 '>
                     <AiOutlineMail size={20}/>
                     <span className='pl-4'>Contact</span>
 
@@ -64,11 +64,11 @@ const SideNav = () => {
                 <AiOutlineFundProjectionScreen size={20}/>
 
             </a>
-            <a href='#main' className='rounded-full shadow-lg bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <a href='/CMS_Resume.pdf' className='rounded-full shadow-lg bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <BsPerson size={20}/>
 
             </a>
-            <a href='#main' className='rounded-full shadow-lg bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <a href='#contact' className='rounded-full shadow-lg bg-gray-500 shadow-gray-600 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <AiOutlineMail size={20}/>
 
             </a>
